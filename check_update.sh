251f60b528c08a2860b1ca4ba67129862643e994
Bernhard Rosenkränzer <bero@lindev.ch>
2022-11-11 01:21:57 +0100

    Remove update checker, there's now a generic version

--- check_update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -L http://mirrors.ctan.org/systems/texlive/tlnet/archive/ 2>/dev/null |grep -E dehyph-exptl.r[0-9]*\.tar |sed -e 's,.*dehyph-exptl\.r,,;s,\.tar.*,,' |sort -V |tail -n1
